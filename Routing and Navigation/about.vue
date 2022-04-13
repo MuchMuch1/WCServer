0c4895c93365e1c82ece132a30e7373a8f032d29
MuchMuch1 <36320799+MuchMuch1@users.noreply.github.com>
2022-04-13 09:52:36 +0800

    Add files via upload

---
+++ Routing and Navigation/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app" style="width: 80%; margin: auto; padding: 30px; color: #eeeeee;">
+      <h1>{{title}}</h1>
+      <h3>Hello! My name is Chadle Rei M. Miclat.</h3>
+      <p>I am a second year student at Holy Angel University. I am currently studying Information Technology, majoring in Web Development.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      title:"About"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Routing and Navigation/contact.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app" style="width: 80%; margin: auto; padding: 30px; color: #eeeeee;">
+      <h1>{{title}}</h1>
+      <p>Contact Us On the Following:</p>
+      <ul>
+        <li>Phone Number: 0936-856-8373</li>
+        <li>Email: cmmiclat@student.hau.edu.ph</li>
+        <li><a href="https://www.facebook.com/chadlerei/">Facebook</a></li>
+        <li><a href="https://www.youtube.com/channel/UCFsuH1WnGcuDb40OK6n-m0w">Youtube</a></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'contact',
+  data () {
+    return {
+      title:"Contact"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
